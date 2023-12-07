b882e1c0073a5ef8e6ec2f9488fd2843b4fbdf3c
djLee77 <dkdnl232@gmail.com>
2023-12-08 01:22:54 +0900

    feat: 웹훅 구현

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,26 @@
-var express = require("express");
-var router = express.Router();
-
-/* GET home page. */
-const app = express();
+var express = require('express');
+var axios = require('axios'); // axios 라이브러리를 불러옵니다.
+var app = express();
 
 app.use(express.static('public'));
 
 app.use('/public', function(req, res, next) {
-  var ip = req.headers['x-forwarded-for'] || req.ip || req.connection.remoteAddress;
-  console.log('IP 주소:', ip);
-  next();
-});
+    var ip = req.headers['x-forwarded-for'] || req.ip || req.connection.remoteAddress;
 
-router.get("/", function (req, res, next) {
-  res.render("index.html");
+    // 디스코드 웹훅 URL
+    var webhookUrl = 'https://discordapp.com/api/webhooks/1182355204079894649/a6eomI7ewgEpQsxfa73IWJC4y1NGJCOIjwvXX0qpVpTwuJSo8s8h0TXCGZZmXnPQRFdr';
+
+    // 디스코드에 보낼 메시지
+    var message = {
+        content: '블로그 접속 감지: IP 주소 - ' + ip
+    };
+
+    // 디스코드 웹훅으로 메시지 보내기
+    axios.post(webhookUrl, message)
+        .then(() => console.log('메시지가 디스코드로 전송됨'))
+        .catch(err => console.error('디스코드로 메시지를 보내는 데 실패함:', err));
+
+    next();
 });
 
 module.exports = router;
