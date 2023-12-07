91cf7fc54ea20e269c0d084ab1828d424233165b
djLee77 <dkdnl232@gmail.com>
2023-12-08 00:35:13 +0900

    refactor : 로컬 이미지 직접 호스팅

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,11 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
+
+app.use(express.static('public'));
+
 router.get("/", function (req, res, next) {
   res.render("index.html");
-});
-
-router.get("/api/get/image", function (req, res, next) {
-  res.send(
-    <img src = "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbbqkOf%2FbtsBfKBtEck%2FC8ipZDOV8uxQ5T9fWFikGK%2Fimg.png"/>
-  );
 });
 
 module.exports = router;
