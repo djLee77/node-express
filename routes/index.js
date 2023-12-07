fa6bd9731a93ae5e014a5747c3e420b6e42d6e2e
djLee77 <dkdnl232@gmail.com>
2023-12-07 23:30:40 +0900

    feat: 이미지 게터 api 구현

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index.html');
 });
 
+router.post('/api/get/image', function(req, res, next) {
+  res.json({
+    status: 200,
+    data: {
+      image: 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbbqkOf%2FbtsBfKBtEck%2FC8ipZDOV8uxQ5T9fWFikGK%2Fimg.png'
+    }
+  });
+});
+
 module.exports = router;
