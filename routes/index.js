be484606aa82aca2f56bd9d2bd2fa8e82f6b8b13
djLee77 <dkdnl232@gmail.com>
2023-12-08 01:06:11 +0900

    feat: 로깅 코드 작성

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,12 @@ const app = express();
 
 app.use(express.static('public'));
 
+app.use('/public', function(req, res, next) {
+  var ip = req.headers['x-forwarded-for'] || req.ip || req.connection.remoteAddress;
+  console.log('IP 주소:', ip);
+  next();
+});
+
 router.get("/", function (req, res, next) {
   res.render("index.html");
 });
