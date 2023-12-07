919cedd06de2989586a75c5841bca4266b8461a8
djLee77 <dkdnl232@gmail.com>
2023-12-08 00:16:22 +0900

    refactor: json 객체에 담지 않고 직접 주소를 반환하도록 수정

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index.html');
+router.get("/", function (req, res, next) {
+  res.render("index.html");
 });
 
-router.get('/api/get/image', function(req, res, next) {
-  res.json({
-    status: 200,
-    data: {
-      image: 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbbqkOf%2FbtsBfKBtEck%2FC8ipZDOV8uxQ5T9fWFikGK%2Fimg.png'
-    }
-  });
+router.get("/api/get/image", function (req, res, next) {
+  res.send(
+    "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbbqkOf%2FbtsBfKBtEck%2FC8ipZDOV8uxQ5T9fWFikGK%2Fimg.png"
+  );
 });
 
 module.exports = router;
