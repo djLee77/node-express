fb8d56c4b07b72e908f1af0102e94534badae559
djLee77 <dkdnl232@gmail.com>
2023-12-08 00:23:41 +0900

    test

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get("/", function (req, res, next) {
 
 router.get("/api/get/image", function (req, res, next) {
   res.send(
-    "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbbqkOf%2FbtsBfKBtEck%2FC8ipZDOV8uxQ5T9fWFikGK%2Fimg.png"
+    <img src = "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbbqkOf%2FbtsBfKBtEck%2FC8ipZDOV8uxQ5T9fWFikGK%2Fimg.png"/>
   );
 });
 
