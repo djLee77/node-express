38459aa583b3b628c03edd21d712ae89a0d19d8d
djLee77 <dkdnl232@gmail.com>
2023-12-09 01:55:10 +0900

    feat: 이미지, 알림 받을 게시글 추가

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,27 @@ app.use("/images/book-logo.png", function (req, res, next) {
 
   // 디스코드에 보낼 메시지
   var message = {
-    content: "블로그 접속 감지: IP 주소 - " + ip,
+    content: "모던 자바스크립트 딥다이브 접속 감지: IP 주소 - " + ip,
+  };
+
+  // 디스코드 웹훅으로 메시지를 보냅니다.
+  axios.post(webhookUrl, message)
+    .then(() => console.log("메시지가 디스코드로 전송됨"))
+    .catch((err) => console.error("디스코드로 메시지를 보내는 데 실패함:", err));
+
+  next();
+});
+
+app.use("/images/iptrack.png", function (req, res, next) {
+  var ip = req.headers["x-forwarded-for"] || req.ip || req.connection.remoteAddress;
+  console.log("ip : ", ip); // 사용자의 IP 주소를 로그에 기록합니다.
+
+  // 디스코드 웹훅 URL
+  var webhookUrl = "https://discordapp.com/api/webhooks/1182355204079894649/a6eomI7ewgEpQsxfa73IWJC4y1NGJCOIjwvXX0qpVpTwuJSo8s8h0TXCGZZmXnPQRFdr";
+
+  // 디스코드에 보낼 메시지
+  var message = {
+    content: "방문 알림 기능 포스팅 접속 감지: IP 주소 - " + ip,
   };
 
   // 디스코드 웹훅으로 메시지를 보냅니다.
